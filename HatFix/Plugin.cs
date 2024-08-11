0f3dfc27195bc24305e12e9cc0a9d0fcd5ccc139
Graze <reece2004f@gmail.com>
2024-08-11 10:32:11 +0100

    Add project files.

---
+++ HatFix/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using GorillaNetworking;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Utilla;
+
+namespace HatFix
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start() =>
+        GorillaTagger.OnPlayerSpawned(delegate
+        {
+            Camera useCam = GorillaTagger.Instance.thirdPersonCamera.GetComponentInChildren<Camera>();
+
+            int mask = Camera.main.cullingMask;
+
+            //creates a new layer mask with JUST that layer in it
+            int metaReportScreen = 1 << LayerMask.NameToLayer("MetaReportScreen");
+            int NoMirror = 1 << LayerMask.NameToLayer("NoMirror");
+            int MirrorOnly = 1 << LayerMask.NameToLayer("MirrorOnly");
+
+            //mask &= ~LAYER  takes away the layer
+            mask &= ~NoMirror;
+            mask &= ~metaReportScreen;
+
+            //mask |= LAYER  adds the layer
+            mask |= MirrorOnly;
+            useCam.cullingMask = mask;
+        });
+    }
+}
---
+++ HatFix/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace HatFix
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.hatfix";
+        public const string Name = "HatFix";
+        public const string Version = "1.0.0";
+    }
+}
